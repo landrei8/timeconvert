6f54229061bb74e3f5d0f6d81a5f736780b2c767
landrei8 <spam.ndy83@proton.me>
2026-03-09 09:39:31 +0200

    Update version.js

--- version.js
+++ version.js
@@ -4,7 +4,7 @@
 // Edit ONLY this file to update the version across all pages.
 // After changing, just refresh any page — it will pick it up automatically..
 
-const APP_VERSION = "v1.8 - 00:29PM EET";
+const APP_VERSION = "v1.8.1 - 00:29PM EET";
 const APP_BUILD_DATE = "March 2026";
 
 // Automatically inject version into any element with class "build-tag"
